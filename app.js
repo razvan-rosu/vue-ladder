95fc66f435a5c69b06253a5a520f5268b790e7d1
Răzvan Roșu <razvan.rosu89@gmail.com>
2017-04-28 03:13:49 +0300

    Add basic file structure

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/images', express.static(__dirname + '/images'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.listen(9999);
+
+console.log('Example app listening on port 9999!');
